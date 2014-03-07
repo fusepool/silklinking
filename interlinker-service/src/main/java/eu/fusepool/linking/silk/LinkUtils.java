558374deec0c2a9f35070cc00cde1bf99a9a7089
Luigi Selmi <selmi_luigi@hotmail.com>
2014-03-07 19:00:47 +0100

    issue #14 added utiity method to remove inferenceable equivalences

---
+++ interlinker-service/src/main/java/eu/fusepool/linking/silk/LinkUtils.java
@@ -0,0 +1,49 @@
+package eu.fusepool.linking.silk;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.clerezza.rdf.core.MGraph;
+import org.apache.clerezza.rdf.core.NonLiteral;
+import org.apache.clerezza.rdf.core.Resource;
+import org.apache.clerezza.rdf.core.Triple;
+import org.apache.clerezza.rdf.core.impl.SimpleMGraph;
+import org.apache.clerezza.rdf.ontologies.OWL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LinkUtils {
+    
+    private static Logger logger = LoggerFactory.getLogger(LinkUtils.class);
+    /**
+     * Removes owl:sameAs triples where subject and object have the same representation
+     * (e.g. <uri1><owl:sameAs><uri1>
+     * Removes triples that can be inferred by similarity property of owl:sameAs. Removes one of
+     * {<uri1><owl:sameAs><uri2>, <uri2><owl:sameAs><uri1>}
+     * @param graph
+     */
+    public static void removeInferenceableEquivalences(MGraph graph) {
+        //removes equivalences that can be inferred by reflexivity of owl:sameAs
+        MGraph reflexGraph = new SimpleMGraph();
+        int initialSize = graph.size();
+        logger.info("Removing inferenceable triple from " + initialSize + " equivalences.");
+        Iterator<Triple> equivalences = graph.filter(null, OWL.sameAs, null);
+        while(equivalences.hasNext()) {
+            Triple equivalence = equivalences.next();
+            Resource subject = (Resource) equivalence.getSubject();
+            Resource object = equivalence.getObject();
+            if(subject.equals(object)){
+                reflexGraph.add(equivalence);
+            } 
+        }
+        
+        graph.removeAll(reflexGraph);
+        logger.info("Removed " + reflexGraph.size() + " inferenceable equivalences by owl:sameAs reflexivity.");
+        
+        //removes equivalences that can be inferred by symmetry of owl:sameAs
+        // TO-DO
+        
+    }
+ 
+}
