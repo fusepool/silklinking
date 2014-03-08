89828ab0a25e9a7a94c190100f47cef6ad6711a7
retobg <reto@apache.org>
2014-03-08 14:01:41 +0100

    formatting

--- silk-fusepool-bundle/src/main/java/eu/fusepool/java/silk/client/impl/SilkClientImpl.java
+++ silk-fusepool-bundle/src/main/java/eu/fusepool/java/silk/client/impl/SilkClientImpl.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package eu.fusepool.java.silk.client.impl;
 
@@ -15,9 +15,6 @@ import org.osgi.service.component.ComponentContext;
 import eu.fusepool.java.silk.client.SilkClient;
 import eu.fusepool.scala.silk.Silk;
 
-
-
-
 /**
  * @author giorgio
  *
@@ -26,77 +23,71 @@ import eu.fusepool.scala.silk.Silk;
 @Service(SilkClient.class)
 public class SilkClientImpl implements SilkClient {
 
-	protected BundleContext ctx ;
-	
-	protected ComponentContext componentContext ;
-	
-	/* (non-Javadoc)
-	 * @see eu.fusepool.java.silk.client.SilkClient#excute()
-	 */
-	public void execute() {
-		InputStream configStream = SilkClientImpl.class.getResourceAsStream("/config/silk-bundle-config2.xml") ;
-		Silk.executeStream(configStream, null, 1, true) ;
-		/*
-		File file = ctx.getDataFile("SilkClientFile.txt") ;
-		if(file!=null) {
-			System.out.println("\n\n######################\n"+file.getAbsolutePath()+"\n###########");
-		} else {
-			System.out.println("\n\n######################\n"+"nessun file creato"+"\n###########");
-		}
-		*/
-	}
+    protected BundleContext ctx;
+    protected ComponentContext componentContext;
 
-	/* (non-Javadoc)
-	 * @see eu.fusepool.java.silk.client.SilkClient#executeStream(java.io.InputStream, java.lang.String, int, boolean)
-	 */
-	public void executeStream(InputStream config, String linkSpecId,
-			int numThreads, boolean reload)  {
-		Silk.executeStream(config, linkSpecId, numThreads, reload) ;
-	}
+    /* (non-Javadoc)
+     * @see eu.fusepool.java.silk.client.SilkClient#excute()
+     */
+    public void execute() {
+        InputStream configStream = SilkClientImpl.class.getResourceAsStream("/config/silk-bundle-config2.xml");
+        Silk.executeStream(configStream, null, 1, true);
+        /*
+         File file = ctx.getDataFile("SilkClientFile.txt") ;
+         if(file!=null) {
+         System.out.println("\n\n######################\n"+file.getAbsolutePath()+"\n###########");
+         } else {
+         System.out.println("\n\n######################\n"+"nessun file creato"+"\n###########");
+         }
+         */
+    }
 
-	/* (non-Javadoc)
-	 * @see eu.fusepool.java.silk.client.SilkClient#executeFile(java.io.File, java.lang.String, int, boolean)
-	 */
-	public void executeFile(File config, String linkSpecId, int numThreads,
-			boolean reload) {
-		Silk.executeFile(config, linkSpecId, numThreads, reload) ;
-	}
+    /* (non-Javadoc)
+     * @see eu.fusepool.java.silk.client.SilkClient#executeStream(java.io.InputStream, java.lang.String, int, boolean)
+     */
+    public void executeStream(InputStream config, String linkSpecId,
+            int numThreads, boolean reload) {
+        Silk.executeStream(config, linkSpecId, numThreads, reload);
+    }
 
-	/* (non-Javadoc)
-	 * @see eu.fusepool.java.silk.client.SilkClient#ExecuteConfig()
-	 */
-	public void executeConfig() throws Exception {
-		// TODO Auto-generated method stub
-		throw new Exception("not implemented yet!") ;
-	}
+    /* (non-Javadoc)
+     * @see eu.fusepool.java.silk.client.SilkClient#executeFile(java.io.File, java.lang.String, int, boolean)
+     */
+    public void executeFile(File config, String linkSpecId, int numThreads,
+            boolean reload) {
+        Silk.executeFile(config, linkSpecId, numThreads, reload);
+    }
 
-	
-	   @Activate
-	   protected void activate(final ComponentContext componentContext) {
-		   this.componentContext = componentContext ;
-		   //this.excute() ;
-	
-	   }
-	
+    /* (non-Javadoc)
+     * @see eu.fusepool.java.silk.client.SilkClient#ExecuteConfig()
+     */
+    public void executeConfig() throws Exception {
+        // TODO Auto-generated method stub
+        throw new Exception("not implemented yet!");
+    }
 
-	
-		/**
-		 * @param args
-		 * @throws Exception 
-		 */
-		public static void main(String[] args) throws Exception {
-			SilkClient client = new SilkClientImpl() ;
-			client.execute() ;
+    @Activate
+    protected void activate(final ComponentContext componentContext) {
+        this.componentContext = componentContext;
+        //this.excute() ;
 
-		}
+    }
 
-		public BundleContext getCtx() {
-			return ctx;
-		}
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        SilkClient client = new SilkClientImpl();
+        client.execute();
 
-		public void setCtx(BundleContext ctx) {
-			this.ctx = ctx;
-		}
-	
-	
+    }
+
+    public BundleContext getCtx() {
+        return ctx;
+    }
+
+    public void setCtx(BundleContext ctx) {
+        this.ctx = ctx;
+    }
 }
